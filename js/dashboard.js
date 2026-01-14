76bab241a490b626aff7b5868aee3b6445825967
wwwmadumalsasidu-hash <www.madumalsasidu@gmail.com>
2026-01-14 15:23:47 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,9 +4,9 @@ import { onAuthStateChanged } from
 import { doc, getDoc } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-onAuthStateChanged(auth, async(user)=>{
-  if(!user) location.href="index.html";
-
-  const snap = await getDoc(doc(db,"users",user.uid));
-  document.getElementById("balance").innerText = snap.data().balance;
+onAuthStateChanged(auth, async (user) => {
+  if (!user) location.href = "index.html";
+  const snap = await getDoc(doc(db, "users", user.uid));
+  document.getElementById("balance").innerText =
+    "Balance: LKR " + snap.data().balance;
 });
