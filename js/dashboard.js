c5664b4a7650e581b05d949e70331b1e55c5b63f
wwwmadumalsasidu-hash <www.madumalsasidu@gmail.com>
2026-01-14 21:59:55 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,29 +4,16 @@ import { onAuthStateChanged } from
 import { doc, getDoc } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const balanceEl = document.getElementById("balance");
-
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
     location.href = "index.html";
     return;
   }
 
-  try {
-    const snap = await getDoc(doc(db, "users", user.uid));
+  const snap = await getDoc(doc(db, "users", user.uid));
 
-    if (snap.exists()) {
-      const data = snap.data();
-      const balance = data.balance || 0;
-
-      // 🔥 BALANCE SHOW
-      balanceEl.innerText = "LKR " + balance;
-    } else {
-      balanceEl.innerText = "LKR 0";
-    }
-
-  } catch (err) {
-    console.error(err);
-    balanceEl.innerText = "LKR 0";
+  if (snap.exists()) {
+    document.getElementById("balance").innerText =
+      "LKR " + snap.data().balance;
   }
 });
