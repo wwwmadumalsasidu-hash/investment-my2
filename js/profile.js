1480753aae3df23f2f18b91df17e06500cb82940
wwwmadumalsasidu-hash <www.madumalsasidu@gmail.com>
2026-01-14 23:26:09 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,33 +1,14 @@
-import { auth, db } from "./firebase.js";
+import { auth } from "./firebase.js";
 import { onAuthStateChanged, signOut } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { doc, getDoc } from
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, (user) => {
   if (!user) {
     location.href = "index.html";
     return;
   }
 
-  // show email
   document.getElementById("userEmail").innerText = user.email;
-
-  try {
-    const userRef = doc(db, "users", user.uid);
-    const snap = await getDoc(userRef);
-
-    if (snap.exists()) {
-      const data = snap.data();
-      document.getElementById("userBalance").innerText =
-        data.balance ?? 0;
-    } else {
-      document.getElementById("userBalance").innerText = 0;
-    }
-  } catch (e) {
-    console.error(e);
-    document.getElementById("userBalance").innerText = 0;
-  }
 });
 
 window.logout = async () => {
