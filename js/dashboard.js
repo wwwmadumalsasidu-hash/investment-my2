dc86d108bd965a82c1c21336e192e6ea1a03529f
wwwmadumalsasidu-hash <www.madumalsasidu@gmail.com>
2026-01-14 14:03:22 +0530

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,12 @@
+import { auth, db } from "./firebase.js";
+import { onAuthStateChanged } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, getDoc } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+onAuthStateChanged(auth, async(user)=>{
+  if(!user) location.href="index.html";
+
+  const snap = await getDoc(doc(db,"users",user.uid));
+  document.getElementById("balance").innerText = snap.data().balance;
+});
