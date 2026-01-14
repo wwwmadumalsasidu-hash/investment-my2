ffbb741c904c08173cfe2752b0c71b1778e4a83a
wwwmadumalsasidu-hash <www.madumalsasidu@gmail.com>
2026-01-14 21:48:16 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,10 @@
 import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { doc, getDoc } from
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const balanceEl = document.getElementById("balance");
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
@@ -10,10 +12,21 @@ onAuthStateChanged(auth, async (user) => {
     return;
   }
 
-  const snap = await getDoc(doc(db, "users", user.uid));
+  try {
+    const snap = await getDoc(doc(db, "users", user.uid));
 
-  if (snap.exists()) {
-    document.getElementById("balanceAmount").innerText =
-      "LKR " + snap.data().balance;
+    if (snap.exists()) {
+      const data = snap.data();
+      const balance = data.balance || 0;
+
+      // 🔥 BALANCE SHOW
+      balanceEl.innerText = "LKR " + balance;
+    } else {
+      balanceEl.innerText = "LKR 0";
+    }
+
+  } catch (err) {
+    console.error(err);
+    balanceEl.innerText = "LKR 0";
   }
 });
